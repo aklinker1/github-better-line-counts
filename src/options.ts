6510f6e6ebc84def04071c6addd5f927c5a88a3a
Aaron Klinker <aklinker1@users.noreply.github.com>
2023-03-05 09:07:40 -0600

    chore: Refactor into OptionsPage and OptionsForm

--- src/options.ts
+++ src/options.ts
@@ -1,4 +1,4 @@
 import { createApp } from "vue";
-import Options from "./components/Options.vue";
+import Options from "./pages/Options.vue";
 
 createApp(Options).mount(document.body);
---
+++ src/pages/Options.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import OptionsForm from "../components/OptionsForm.vue";
+</script>
+
+<template>
+  <Suspense>
+    <OptionsForm />
+  </Suspense>
+</template>
