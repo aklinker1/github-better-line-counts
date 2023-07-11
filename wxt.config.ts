55c1b96a3efaab521e58541f2548dfc025dfd530
Aaron Klinker <aklinker1@users.noreply.github.com>
2023-07-11 17:56:11 -0500

    It's working

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "wxt";
+import { UserManifest, defineConfig } from "wxt";
 import Vue from "@vitejs/plugin-vue";
 import Icons from "unplugin-icons/vite";
 
@@ -11,19 +11,23 @@ export default defineConfig({
       { from: "vue-query", name: "useMutation" },
     ],
   },
-  // TODO: Switch to vite.UserConfig
   vite: {
     plugins: [Icons({ compiler: "vue3" }), Vue()],
   },
-  manifest: {
-    icons: {
-      "16": "icon/16.png",
-      "32": "icon/32.png",
-      "48": "icon/48.png",
-      "96": "icon/96.png",
-      "128": "icon/128.png",
-    },
-    // TODO: Only apply api.github.com permission to firefox
-    permissions: ["storage", "https://api.github.com/*"],
+  manifest: ({ browser }) => {
+    const manifest: UserManifest = {
+      icons: {
+        "16": "icon/16.png",
+        "32": "icon/32.png",
+        "48": "icon/48.png",
+        "96": "icon/96.png",
+        "128": "icon/128.png",
+      },
+      permissions: ["storage"],
+    };
+    if (browser === "firefox") {
+      manifest.permissions!.push("https://api.github.com/*");
+    }
+    return manifest;
   },
 });
