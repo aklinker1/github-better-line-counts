b038a0cd6fd9d3bb614babc0eec0eae51a47f2e6
Aaron Klinker <aklinker1@users.noreply.github.com>
2023-07-10 22:42:32 -0500

    Add more imports

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,11 +4,19 @@ import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
   srcDir: "src",
+  imports: {
+    presets: ["vue", "vue-router", "@vueuse/core"],
+    imports: [
+      { from: "vue-query", name: "useQuery" },
+      { from: "vue-query", name: "useMutation" },
+    ],
+  },
   // TODO: Switch to vite.UserConfig
   vite: {
     plugins: [Icons({ compiler: "vue3" }), Vue()],
   },
   manifest: {
+    // TODO: Allow icon customization
     // TODO: Only apply api.github.com permission to firefox
     permissions: ["storage", "https://api.github.com/*"],
   },
